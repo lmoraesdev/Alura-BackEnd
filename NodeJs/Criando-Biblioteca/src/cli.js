3e417ae62c62bad0edd0a2460cf8ef46d398c14c
lmoraesdev <lbc_moraes@outlook.com>
2022-11-21 16:32:04 -0300

    refactor: imprimeLista

--- NodeJs/Criando-Biblioteca/src/cli.js
+++ NodeJs/Criando-Biblioteca/src/cli.js
@@ -4,20 +4,32 @@ import pegaArquivo from "./index.js";
 
 const caminho = process.argv;
 
-function imprimeLista(resultado) {
-	console.log(chalk.yellow("lista de links"), resultado);
+function imprimeLista(resultado, indeficador = '') {
+	console.log(
+		chalk.yellow("lista de links"),
+		chalk.black.bgGreen(indeficador),
+		 resultado);
 }
 
 async function processaTexto(argumentos) {
 	const caminho = argumentos[2];
+	try {
+		fs.lstatSync(caminho);
+	} catch (erro) {
+		if (erro.code === 'ENOENT') {
+			console.log(chalk.red('arquivo ou diretório não existe'))
+			return
+		}
+	}
+
 	if (fs.lstatSync(caminho).isFile()) {
 		const resultado = await pegaArquivo(argumentos[2]);
-		imprimeLista(resultado);
+		imprimeLista(resultado, );
 	} else if (fs.lstatSync(caminho).isDirectory()) {
 		const arquivos = await fs.promises.readdir(caminho);
 		arquivos.forEach(async (nomeDeArquivo) => {
 			const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
-			imprimeLista(lista);
+			imprimeLista(lista, nomeDeArquivo);
 		});
 	}
 }
