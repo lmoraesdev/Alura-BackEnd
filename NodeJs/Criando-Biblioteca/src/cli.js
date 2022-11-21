65423177f701f747c609d9c74da704a3632e96ec
lmoraesdev <lbc_moraes@outlook.com>
2022-11-21 16:56:50 -0300

    feat: http-validacao

--- NodeJs/Criando-Biblioteca/src/cli.js
+++ NodeJs/Criando-Biblioteca/src/cli.js
@@ -1,35 +1,46 @@
 import chalk from "chalk";
 import fs from "fs";
 import pegaArquivo from "./index.js";
+import listaValidada from "./http-validacao.js";
 
 const caminho = process.argv;
 
-function imprimeLista(resultado, indeficador = '') {
-	console.log(
-		chalk.yellow("lista de links"),
-		chalk.black.bgGreen(indeficador),
-		 resultado);
+function imprimeLista(valida, resultado, indeficador = "") {
+	if (valida) {
+		console.log(
+			chalk.yellow("lista validada"),
+			chalk.black.bgGreen(indeficador),
+			listaValidada(resultado)
+		);
+	} else {
+		console.log(
+			chalk.yellow("lista de links"),
+			chalk.black.bgGreen(indeficador),
+			resultado
+		);
+	}
 }
 
 async function processaTexto(argumentos) {
 	const caminho = argumentos[2];
+	const valida = argumentos[3] === '--valida';
 	try {
 		fs.lstatSync(caminho);
 	} catch (erro) {
-		if (erro.code === 'ENOENT') {
-			console.log(chalk.red('arquivo ou diretório não existe'))
-			return
+		if (erro.code === "ENOENT") {
+			console.log(chalk.red("arquivo ou diretório não existe"));
+			return;
 		}
 	}
 
 	if (fs.lstatSync(caminho).isFile()) {
 		const resultado = await pegaArquivo(argumentos[2]);
-		imprimeLista(resultado, );
+		imprimeLista(valida, resultado);
 	} else if (fs.lstatSync(caminho).isDirectory()) {
 		const arquivos = await fs.promises.readdir(caminho);
 		arquivos.forEach(async (nomeDeArquivo) => {
 			const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
-			imprimeLista(lista, nomeDeArquivo);
+			imprimeLista(valida, lista, nomeDeArquivo);
 		});
 	}
 }
