f85c3e137a17dd2aced501309e2b3bd37f04d027
lmoraesdev <lbc_moraes@outlook.com>
2022-11-24 21:25:39 -0300

    finalizado

--- NodeJs/javaScript-orientadaObjetos/User.js
+++ NodeJs/javaScript-orientadaObjetos/User.js
@@ -1,29 +1,51 @@
 export default class User {
-	#nome;
-	#email;
-	#nascimento;
-	#role;
-	#ativo;
+	_nome;
+	_email;
+	_nascimento;
+	_role;
+	_ativo;
 	constructor(nome, email, nascimento, role, ativo = true) {
-		this.#nome = nome;
-		this.#email = email;
-		this.#nascimento = nascimento;
-		this.#role = role || "estudante";
-		this.#ativo = ativo;
+		this._nome = nome;
+		this._email = email;
+		this._nascimento = nascimento;
+		this._role = role || "estudante";
+		this._ativo = ativo;
+	}
+	get nome() {
+		return this._nome;
+	}
+	get email() {
+		return this._email;
+	}
+	get nascimento() {
+		return this._nascimento;
+	}
+	get role() {
+		return this._role;
+	}
+	get ativo() {
+		return this._ativo;
 	}
 
-	#montaObjUser() {
+	set nome(novoNome){
+		if (novoNome === ''){
+			throw new Error('Formato não válidaD')
+		}
+		this._nome = novoNome
+	}
+
+	_montaObjUser() {
 		return {
-			name: this.#nome,
-			email: this.#email,
-			Nascimento: this.#nascimento,
-			role: this.#role,
-			ativo: this.#ativo,
+			name: this._nome,
+			email: this._email,
+			Nascimento: this._nascimento,
+			role: this._role,
+			ativo: this._ativo,
 		};
 	}
 
 	exibirInfos() {
-		const objUser = this.#montaObjUser();
+		const objUser = this._montaObjUser();
 		return `${objUser.nome}, ${objUser.email},${objUser.nascimento},${objUser.role},${objUser.ativo}`;
 	}
 }
